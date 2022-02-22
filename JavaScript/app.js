69fb23213090e99db1d54fffc78c33a61cd19191
Joaquin-Ojeda <joaquin_ema_ojeda@hotmail.com>
2022-02-22 19:36:39 -0300

    ArreglandoErrores

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,13 +1,11 @@
-var pulsar=1;
 function divLogin(num){
-    if(pulsar==1){
-        document.getElementById("caja"+num).style.visibility="visible";
-        document.getElementById("caja"+num).style.display="block";
-        pulsar=0;
-    }
-    else{
+    if(document.getElementById("caja"+num).style.visibility=="visible"){
         document.getElementById("caja"+num).style.visibility="hidden";
         document.getElementById("caja"+num).style.display="none";
-        pulsar=1;
+    }
+    else{
+        document.getElementById("caja"+num).style.visibility="visible";
+        document.getElementById("caja"+num).style.display="block";
+
     }
 }
